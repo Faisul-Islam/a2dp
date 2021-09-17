8deed184f93afc8da9608f7ba58bf143538984a6
Faisul Islam <suhid15@gmail.com>
2021-09-18 02:31:13 +0600

    Add files via upload

---
+++ precache-manifest.e26fdce619418d4962635776ca1b6428.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6ee54fe1b16b7bbacb",
+    "url": "/css/about.d1175965.css"
+  },
+  {
+    "revision": "aa160d78ea2a9d9ec742",
+    "url": "/css/app.696c285f.css"
+  },
+  {
+    "revision": "749553afb0566380f52f",
+    "url": "/css/chunk-vendors.dd463983.css"
+  },
+  {
+    "revision": "524e60cd3ebcce32be8c66eee8492f48",
+    "url": "/img/hero-logo.524e60cd.png"
+  },
+  {
+    "revision": "772aa6d190ba705e267607434a561fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6ee54fe1b16b7bbacb",
+    "url": "/js/about.2c4b1f25.js"
+  },
+  {
+    "revision": "aa160d78ea2a9d9ec742",
+    "url": "/js/app.51c2f06b.js"
+  },
+  {
+    "revision": "749553afb0566380f52f",
+    "url": "/js/chunk-vendors.8aef150e.js"
+  },
+  {
+    "revision": "e99759910e649343d8c066b7c5805b7f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3cb2f186f06208bb33dacf671a6d1225",
+    "url": "/scripts/jsstore.worker.min.a018673d044e5e68dcf910e83369d183.js"
+  }
+]);
